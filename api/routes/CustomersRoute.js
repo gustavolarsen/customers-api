2d770e1cff2884f77f2ef2241c3daa219f1d6257
Gustavo Larsen <41484330+gusgamedev@users.noreply.github.com>
2020-02-10 22:59:03 -0300

    add rota post

--- api/routes/CustomersRoute.js
+++ api/routes/CustomersRoute.js
@@ -2,6 +2,9 @@ module.exports = app => {
 
     const controller = app.controllers.CustomersController;
 
-    app.route('/api/v1/customers').get(controller.listCustomers);
+    app.route('/api/v1/customers')
+        .get(controller.listCustomers)
+        .post(controller.saveCustomeres);
+
 
 }
